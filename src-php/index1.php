e3e5e9dcf906377cda7315b5b9d235556e2c9ae2
Marcos Chamorro <marcoschamorro41@gmail.com>
2016-11-02 21:31:31 -0700

    login

---
+++ src-php/index1.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="UTF-8">
+        <title>Login de Usuario</title>
+        <link rel="stylesheet" href="./css/bootstrap.css" media="screen" type="text/css" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.13.1/jquery.validate.min.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+ 
+    </head>
+
+
+ <body>
+<div class="container">
+    <div class="row">
+  <div class="col-md-4 col-md-offset-4">
+      <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3 class="panel-title">Por favor complete los campos</h3>
+     </div>
+      <div class="panel-body">
+        <form accept-charset="UTF-8" role="form">
+                    <fieldset>
+           <div class="form-group">
+             <input class="form-control" placeholder="E-mail" name="email" type="text">
+         </div>
+         <div class="form-group">
+          <input class="form-control" placeholder="Contraseña" name="password" type="password" value="">
+         </div>
+         <input class="btn btn-lg btn-success btn-block" type="submit" value="Ingresar">
+        </fieldset>
+          </form>
+       </div>
+   </div>
+  </div>
+ </div>
+</div>
+ </body>
+ </html>
